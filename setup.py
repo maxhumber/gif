c749ec651970d3f8f6fd73f25a879ebff0d979fc
Max Humber <max.humber@gmail.com>
2020-01-31 22:48:47 -0500

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="gif",
-    version="1.0.1",
+    version="1.0.2",
     description="✨ Better animated Matplotlib gifs",
     long_description=long_description,
     long_description_content_type="text/markdown",
